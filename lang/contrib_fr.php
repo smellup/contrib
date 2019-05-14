2c1010e59ca99f626b03a2df1e077d8b9ddb2c43
Eric Lupinacci <eric@smellup.net>
2019-05-14 08:51:50 +0200

    Mise au point des items de langue

--- lang/contrib_fr.php
+++ lang/contrib_fr.php
@@ -8,11 +8,14 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'categorie_vide' => '--',
+	'categorie_vide_label' => 'Pas de catégorie',
 	'configuration_page_titre' => 'Configuration du plugin Contrib',
 	'configuration_secteur_galaxie_label' => 'Secteurs disponibles',
 	'configuration_secteur_galaxie_explication' => 'Choisir parmi les secteurs disponibles (hors secteur-carnet, secteur-apropos et secteurs-plugin) le ou les secteurs qui composeront la partie Galaxie du site.',
 
 	// L
-	'label_type_article' => 'Type d\'article',
+	'type_article_label' => 'Type d\'article',
+	'type_article_utilisation_label' => 'documentation générale',
+	'type_article_conception_label' => 'documentation de conception',
+	'type_article_actualite_label' => 'article d\actualité',
 );
