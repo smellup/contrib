795ec1a0eea78d501df63e9cc92e32417bb56c7e
Eric Lupinacci <eric@smellup.net>
2019-08-11 21:02:44 +0200

    utilisation du plugin couleur_objet pour ajouter une couleur aux catégories. On pourra ainsi supprimer la couleur des rubriques qui en héritera via sa catégorie. Broutille de PSR

--- contrib_pipelines.php
+++ contrib_pipelines.php
@@ -16,3 +16,36 @@ function contrib_formulaire_charger($flux) {
 
 	return $flux;
 }
+
+function contrib_affiche_droite($flux) {
+
+	// Identification de la page et de l'objet
+	$exec = $flux['args']['exec'];
+
+	// Récupérer la liste des objets qui supporte une couleur
+	include_spip('inc/config');
+	$objets_config = lire_config('couleur_objet/objets', array());
+
+	if (
+		in_array('spip_mots', $objets_config) // si configuration objets ok
+		and ($exec == 'type_plugin') // page d'un objet éditorial
+		and ($id_objet = intval($flux['args']['id_mot']))
+	) {
+		$couleur = sql_getfetsel(
+			'couleur_objet',
+			'spip_couleur_objet_liens',
+			array(
+				'objet=' . sql_quote('mot'),
+				'id_objet=' . $id_objet
+			)
+		);
+		$contexte = array(
+			'objet'         => 'mot',
+			'id_objet'      => $id_objet,
+			'couleur_objet' => $couleur
+		);
+		$flux['data'] .= recuperer_fond('inclure/couleur_objet', $contexte);
+	}
+
+	return $flux;
+}
