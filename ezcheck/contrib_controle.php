cc2e4bd615d3c4bd9ec3f3c68227bb17c148bdf6
Eric Lupinacci <eric@smellup.net>
2020-01-30 20:35:07 +0100

    Renommage des contrôles pour suivre les modifications de ezcheck. Ajout du fichier des fonctions de contrôle.

---
+++ ezcheck/contrib_controle.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function contrib_article_prepa($id_controle, $id_auteur) {
+
+
+	return true;
+}
+
+function contrib_rubrique_prefixe($id_controle, $id_auteur) {
+
+
+	return true;
+}
